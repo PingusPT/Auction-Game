6fc4e65d58ee1f387ca9b8fda29f5709634c64e2
PingusPT <122572061+PingusPT@users.noreply.github.com>
2024-05-24 16:00:35 +0100

    Final commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,14 @@
     }
 </script>
 
-<h1>Auction Game</h1>
+<h1 class="font-extrabold text-purple-950 border-8 pt-2.5 pb-2.5 border-purple-950 bg-fuchsia-200 rounded-3xl mt-14 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+    Auction Game
+</h1>
 
-<div class="flex items-center justify-center flex-wrap">
-<button on:click={navigateToGame} class="rounded-lg bg-fuchsia-200 h-24 w-44 text-4xl font-extrabold text-purple-950 border-dashed">
-    Enter The Auction
-</button>
+<div class="flex items-center justify-center h-screen">
+    <button on:click={navigateToGame} class="rounded-lg bg-fuchsia-200 text-4xl font-extrabold text-purple-950 hover:text-purple-600 hover:border-purple-700 border-purple-950 border-8 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl h-auto ">
+        Enter The Auction
+    </button>
 </div>
 
 <div class="background-image"></div>
